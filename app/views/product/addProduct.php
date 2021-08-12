71a2b6a28cfda392f23acbdbbd48cd75de352a48
ravihidayat <naufalravi@graduate.utm.my>
2021-08-12 21:48:01 +0700

    addProduct form initial code

--- app/views/product/addProduct.php
+++ app/views/product/addProduct.php
@@ -24,6 +24,23 @@
         <hr>
     </section>
 
-    <section id=" body">
+    <section id="body">
+        <form action="" method="post" id="product_form">
+            <label for="sku">SKU</label>
+            <input type="text" name="sku" id="sku"><br><br>
 
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name"><br><br>
+
+            <label for="price">Price</label>
+            <input type="number" name="price" id="price"><br><br><br>
+
+            <label for="switcher">Type Switcher</label>
+            <select name="switcher" id="productType">
+                <option value="">Type Switcher</option>
+                <option value="DVD" id="DVD">DVD</option>
+                <option value="Furniture" id="Furniture">Furniture</option>
+                <option value="Book" id="Book">Book</option>
+            </select>
+        </form>
     </section>
