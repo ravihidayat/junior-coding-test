91c58987a9670d20948a8ed4c51867c01830404d
ravihidayat <naufalravi@graduate.utm.my>
2021-08-18 14:58:42 +0700

    Refactor script.js and create Procfile

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     $('#productType').change(function() {
         const id = $(this).children(":selected").attr("id");
         $("#switch").load(`app/views/layouts/${id}.php`);
