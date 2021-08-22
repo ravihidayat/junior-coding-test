4782f0ff07098abfed616eb4d85e21476ee4d016
ravihidayat <naufalravi@graduate.utm.my>
2021-08-22 17:17:54 +0700

    Fix constants again

--- app/config/Constants.php
+++ app/config/Constants.php
@@ -10,13 +10,13 @@ class Constants
     const PUSH = 'add-product';
     const POP = './';
     const BASEURL = 'http://ravihidayat.herokuapp.com/';
-    // const DB_HOST = 'us-cdbr-east-04.cleardb.com';
-    // const DB_NAME = 'heroku_47a692aaff029e1';
-    // const DB_USERNAME = 'b0829fcbf6c90d';
-    // const DB_PASSWORD = '12590764';
+    const DB_HOST = 'us-cdbr-east-04.cleardb.com';
+    const DB_NAME = 'heroku_47a692aaff029e1';
+    const DB_USERNAME = 'b0829fcbf6c90d';
+    const DB_PASSWORD = '12590764';
     const CHARSET = 'utf8mb4';
-    const DB_HOST = 'localhost';
-    const DB_NAME = 'phpmvc';
-    const DB_USERNAME = 'root';
-    const DB_PASSWORD = '';
+    // const DB_HOST = 'localhost';
+    // const DB_NAME = 'phpmvc';
+    // const DB_USERNAME = 'root';
+    // const DB_PASSWORD = '';
 }
